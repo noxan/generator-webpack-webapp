83253c65a6cb2c15f22cc0123d6174057806b20d
Richard <richard@devisio.net>
2015-06-15 13:42:39 +0200

    add angular module definition to app.js

--- app/index.js
+++ app/index.js
@@ -38,7 +38,7 @@ module.exports = generators.Base.extend({
     this.fs.copy(this.templatePath("webpack.config.js"), this.destinationPath("webpack.config.js"));
     this.fs.copy(this.templatePath("_jscsrc"), this.destinationPath(".jscsrc"));
 
-    this.fs.copy(this.templatePath("src/app.js"), this.destinationPath("src/app.js"));
+    this.fs.copyTpl(this.templatePath("src/app.js"), this.destinationPath("src/app.js"), this.context);
     this.fs.copy(this.templatePath("src/main.styl"), this.destinationPath("src/main.styl"));
     this.fs.copyTpl(this.templatePath("src/index.html.jade"), this.destinationPath("src/index.html.jade"), this.context);
   },
--- app/templates/src/app.js
+++ app/templates/src/app.js
@@ -1,5 +1,11 @@
 require("./index.html.jade");
 require("./main.styl");
 
+<% if (includeAngular) { %>
+var angular = require("angular");
+
+angular.module = angular.module("<%= appname %>", [
+]);<% } %>
+
 
 console.log("hello world");
