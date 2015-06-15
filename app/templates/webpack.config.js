1fa1df5d9c6d8ad83c92b02422cbd16ac56b78de
Richard <richard@devisio.net>
2015-06-15 13:27:33 +0200

    setup some webpack loaders (stylus, jade, json)

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
   output: {
     path: "./dist/",
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: "json-loader" },
+      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
+      { test: /\.tmpl\.jade$/, loader: "jade-loader" },
+      { test: /index\.html\.jade$/, loader: "file-loader?name=[path][name]&context=./src!jade-html-loader" }
+    ]
   }
 };
