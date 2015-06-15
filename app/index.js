c0986cccc95fdeb6c5601b1036b38a97d7e13889
Richard <richard@devisio.net>
2015-06-15 12:18:45 +0200

    add prompt for appname and copy readme file with proper title

--- app/index.js
+++ app/index.js
@@ -1,3 +1,23 @@
 var generators = require('yeoman-generator');
 
-module.exports = generators.Base.extend();
+
+module.exports = generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    var prompts = [{
+      name: "appname",
+      message: "What's the name of your project?",
+      default: this.appname
+    }];
+
+    this.prompt(prompts, function (answers) {
+      this.props = answers;
+      this.log(answers);
+      done();
+    }.bind(this));
+  },
+  writing: function () {
+    this.fs.copyTpl(this.templatePath("README.md"), this.destinationPath("README.md"), this.props);
+  }
+});
