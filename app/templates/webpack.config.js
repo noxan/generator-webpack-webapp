43a2e9e13aeb2804a499b668f1720efd127d4c45
Richard <richard@devisio.net>
2015-06-15 13:28:53 +0200

    add jshint and jscs loaders

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -12,6 +12,15 @@ module.exports = {
       { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
       { test: /\.tmpl\.jade$/, loader: "jade-loader" },
       { test: /index\.html\.jade$/, loader: "file-loader?name=[path][name]&context=./src!jade-html-loader" }
-    ]
+    ],
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "jscs-loader"
+    },  {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "jshint-loader"
+    }]
   }
 };
